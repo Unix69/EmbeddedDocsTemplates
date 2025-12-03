1b9cc2d7b42f571a1dc90560f780381549013efe
unix69 <giuseppe.pedone.developer@gmail.com>
2025-12-03 01:50:26 +0100

    Modified dinymic linking for GitHub Pages and Doxygen compatibility

--- docs/html/link.js
+++ docs/html/link.js
@@ -20,12 +20,4 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
-
-    // Menu directory-tree espandibile/collassabile
-  document.querySelectorAll(".directory-tree .folder").forEach(folder => {
-    folder.addEventListener("click", e => {
-      e.stopPropagation(); // Previene click sui parent
-      folder.classList.toggle("expanded"); // <-- cambia open -> expanded
-    });
-  });
 });
